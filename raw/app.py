c341f2d6e2b895dad5651ac8c3b2dd061f302941
Tymon Wisniewski <tymonwis.dev@gmail.com>
2024-08-06 13:32:57 +0200

    starting version

---
+++ raw/app.py
@@ -0,0 +1,6 @@
+import random
+
+with open('mapka.txt', 'r') as file:
+    lines = file.readlines()
+    third_line = lines[random.randint(0,20)] #reads the 3rd line in the doc
+    print(third_line)
