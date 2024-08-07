5d654e6daf2483da9896cff23f766aae72b7a722
Tymon Wisniewski <tymonwis.dev@gmail.com>
2024-08-07 23:37:13 +0200

    przeciaganie itemów

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ startBtn.addEventListener('click', (e) => {
 /* console.log('OBJ: ', mapObjectList[Math.floor(Math.random() * (mapObjectList.length-1))], " ", i) */
             const li = document.createElement('div')
             li.classList.add('list-item')
-            const markup = `<span class="drag-number">${i+1}</span><span>${mapObjectList[Math.floor(Math.random() * (mapObjectList.length-1))]}</span>`
+            const markup = `<span id="DN${i}"class="drag-number" draggable="true" ondragstart="drag(event)">${i+1}</span><span>${mapObjectList[Math.floor(Math.random() * (mapObjectList.length-1))]}</span>`
             console.log("markup: ", markup)
             li.innerHTML = markup
             console.log('li:', li)
@@ -32,4 +32,22 @@ startBtn.addEventListener('click', (e) => {
 
 })
 
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drop(ev) {
+    ev.preventDefault();
+/*     console.log("DROP: ", ev) */
+    console.log("pgX", ev.pageX)
+    console.log("pgY", ev.pageY)
+    const newDiv = document.getElementById(ev.dataTransfer.getData("text"))
+    newDiv.style.position = "absolute";
+    newDiv.style.left = ev.pageX+'px';
+    newDiv.style.top = ev.pageY+'px';
+    ev.target.appendChild(newDiv);
+}
 console.log("after: ", mapObjectList)
