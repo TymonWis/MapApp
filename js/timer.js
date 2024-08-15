6039bd236327710a3f4919aa1c23a0051288df8e
Tymon Wisniewski <tymonwis.dev@gmail.com>
2024-08-15 14:11:29 +0200

    zmiany w timerze

--- js/timer.js
+++ js/timer.js
@@ -1,9 +1,10 @@
+let time = 10
 function changeTime(e){
-    var time = e.target.value*60
+    time = e.target.value*60
     if(time > 3599){time = 3599}
     console.log("new time: ", time)
     displayTime(time)
-    document.getElementById('timer').value = time
+    /* document.getElementById('timer').value = time */
 }
 function displayTime(time){
     document.getElementById('timer').innerHTML = ("0"+ Math.floor(time/60)).slice(-2) + ':' + ("0"+time%60).slice(-2)
@@ -14,16 +15,16 @@ function minMaxtime(){
   if (v > 60) this.value = 60;
 }
 function startTimer(){
-    var time2 = document.getElementById('timer').value
-    if(time2 === undefined || time2 <= 0){
+    /* var time2 = document.getElementById('timer').value */
+/*     if(time2 === undefined || time2 <= 0){
         time2 = 10
     }
-
+ */
     var interval = setInterval(() => {
-        displayTime(time2);
-        time2 -= 1;
+        displayTime(time);
+        time -= 1;
 
-        if (time2 < 0) {
+        if (time < 0) {
             clearInterval(interval);
 
             for(let i=0; i <=14; i++){
