8d71b1e861d283639479d44e95257e3e1a4fba26
Tymon Wisniewski <tymonwis.dev@gmail.com>
2024-08-23 22:11:40 +0200

    przed zmiana src na json

--- raw/app.py
+++ raw/app.py
@@ -1,6 +1,6 @@
 import random
 
-with open('mapka.txt', 'r') as file:
+with open('./src.txt', 'r') as file:
     lines = file.readlines()
     third_line = lines[random.randint(0,20)] #reads the 3rd line in the doc
     print(third_line)
