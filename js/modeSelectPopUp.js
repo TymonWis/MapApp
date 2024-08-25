81100bfd9f09d81ff1a89b043c14f3582a5c5889
Tymon Wisniewski <tymonwis.dev@gmail.com>
2024-08-25 23:21:20 +0200

    popup zmiany trybu zmiana trybu

--- js/modeSelectPopUp.js
+++ js/modeSelectPopUp.js
@@ -6,23 +6,23 @@ function getPopUp(){
             <span class="startTitle">WYBIERZ TRYB</span>
             </div>
             <div id="button-container" class="button-container flex-col">
-                <div class="selected">
+                <div id="KL1FIZ" class="selected">
                     <span>KLASA 1</span>
                     <span>MAPA FIZYCZNA</span>
                 </div>
-                <div>
+                <div id="KL2FIZ">
                     <span>KLASA 2</span>
                     <span>MAPA FIZYCZNA</span>
                 </div>
-                <div>
+                <div id="KL2POL">
                     <span>KLASA 2</span>
                     <span>MAPA POLITYCZNA</span>
                 </div>
-                <div>
+                <div id="KL3FIZ">
                     <span>KLASA 3</span>
                     <span>MAPA FIZYCZNA</span>
                 </div>
-                <div>
+                <div id="KL3POL">
                     <span>KLASA 3</span>
                     <span>MAPA POLITYCZNA</span>
                 </div>
@@ -32,6 +32,23 @@ function getPopUp(){
 }
 function showPopUp(){
     document.getElementById('pop-up-place').innerHTML = getPopUp()
-  //  document.querySelector('button-container')
+    document.getElementById('button-container').addEventListener('click', function(e){
+        console.log('I am clicking', e.target.parentNode)
+        console.log('id:', e.target.id)
+        /* lang = e.target.id
+        console.log('lang', lang) */
+        if(e.target.id === 'button-container'){
+            return
+        } else {
+            if(e.target.id === ''){
+            document.querySelector('div.selected').classList.remove('selected')
+            document.getElementById(`${e.target.parentNode.id}`).classList.add('selected')
+            }
+            else{
+            document.querySelector('div.selected').classList.remove('selected')
+            document.getElementById(`${e.target.id}`).classList.add('selected')
+            }
+        }
+        })
 }
 export default{showPopUp}
